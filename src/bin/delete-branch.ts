863da635f3b49549ebdedeffe112f7c455908fe7
Michael Shick <m@shick.us>
2023-03-07 16:39:05 -0500

    support deleting branches as a standalone

--- src/bin/delete-branch.ts
+++ src/bin/delete-branch.ts
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 
+import minimist from 'minimist'
 import { getClient } from '../lib/client.js'
 import { DEVELOPMENT } from '../lib/constants.js'
 import { getConfig, logWithPrefix as log } from '../lib/util.js'
 
-const { apiKey, projectId, buildGitCommitRef } = getConfig()
+const { apiKey, projectId } = getConfig()
+const argv = minimist(process.argv.slice(2))
+const branchName = argv['name']
 
 async function main() {
   if (!apiKey) {
@@ -12,12 +15,16 @@ async function main() {
     return
   }
 
+  if (!branchName) {
+    log('Must provide a --name')
+  }
+
   log('Deleting API branch...')
 
   const client = getClient({ apiKey })
 
   const result = await client.deleteBranch({
-    input: { projectId, environment: DEVELOPMENT, branchName: buildGitCommitRef },
+    input: { projectId, environment: DEVELOPMENT, branchName },
   })
 
   if (result?.deletedBranch) {
