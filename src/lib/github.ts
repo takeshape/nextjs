347d8e71edaa9967baa2ef364160a5abb8b2f731
Michael Shick <m@shick.us>
2023-03-21 10:48:52 -0400

    provide a better message for lookup-pr errors

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,3 +1,4 @@
+import { RequestError } from '@octokit/request-error'
 import { Octokit } from 'octokit'
 import { log } from './log.js'
 
@@ -7,19 +8,33 @@ export async function getHeadRefFromCommitPullsList(
   repo: string,
   commitSha: string,
 ): Promise<string | undefined> {
-  const commitPullsList = await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
-    owner,
-    repo,
-    commit_sha: commitSha,
-  })
+  try {
+    const commitPullsList = await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
+      owner,
+      repo,
+      commit_sha: commitSha,
+    })
 
-  log.debug(JSON.stringify(commitPullsList, null, 2))
+    log.debug(JSON.stringify(commitPullsList, null, 2))
 
-  const commitPull = commitPullsList.data?.[0]
+    const commitPull = commitPullsList.data?.[0]
 
-  if (!commitPull?.merged_at) {
-    return
+    if (!commitPull?.merged_at) {
+      return
+    }
+
+    return commitPull.head.ref
+  } catch (error) {
+    log.debug(error)
+
+    if (error instanceof RequestError) {
+      if (error.status === 404) {
+        throw new Error(
+          'Could not load GitHub PRs. If this is a private repo be sure GITHUB_TOKEN is defined and has proper permissions',
+        )
+      }
+    }
+
+    throw error
   }
-
-  return commitPull.head.ref
 }
